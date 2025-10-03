e3279e0fb6044be0f8edbcf12a0f7d0aa1d8bf03
Rifki Budiyanto <167438084+Rifkibudiyanto@users.noreply.github.com>
2025-10-03 10:11:26 +0700

    .

---
+++ LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak Kami - LaraPress</title>
+</head>
+<body>
+    <h1>Kontak Kami</h1>
+    <p>Silakan hubungi kami melalui informasi berikut:</p>
+    
+    <p><strong>Email:</strong> larapress@example.com</p>
+    <p><strong>Telepon:</strong> +62 812-3456-7890</p>
+</body>
+</html>
